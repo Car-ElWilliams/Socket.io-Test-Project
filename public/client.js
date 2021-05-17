c6e2aee15aee0704af5cbbedbdd5ba68449aced8
Hampus Aronsson <hampzz99@gmail.com>
2021-05-17 20:50:22 +0200

    asd

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
-let socekt = io();
+let socket = io();
 
 socket.on('PlayerLeft', msg => {
-	alert('Player left');
+	console.log('Player left');
 	console.log('Client says hello there)', msg);
 
 	let span = document.querySelector('h1');
