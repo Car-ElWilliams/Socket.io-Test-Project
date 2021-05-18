611065bc4ea35785ca7ab84dd93f050e888215c2
Carel Williams <carel@live.se>
2021-05-18 19:35:19 +0200

    adds questions to client

--- index.js
+++ index.js
@@ -10,17 +10,41 @@ const io = require('socket.io')(httpServer);
 
 const { default: axios } = require('axios');
 
-async function getQuiz() {
+async function getQuiz(round) {
+	let rounds = round;
+
+	let quizQuestion = '';
+	let quizIncorrectAnswers = [];
+	let quizCorrectAnswer = [];
+	let allOptions;
+
 	try {
 		const response = await axios.get('https://opentdb.com/api.php?amount=5&type=multiple');
-		console.log(response.data.results);
-		return response.data.results[0].question;
+		//console.log(response.data.results[rounds]);
+
+		// Quiz question, answer and incorrect answers
+		quizQuestion = response.data.results[rounds].question;
+		quizCorrectAnswer = response.data.results[rounds].correct_answer;
+		quizIncorrectAnswers = response.data.results[rounds].incorrect_answers;
+
+		allOptions = quizIncorrectAnswers.concat(quizCorrectAnswer);
+
+		//console.log(
+		//	'Question is:',
+		//	quizQuestion,
+		//	'Answer is:',
+		//	quizCorrectAnswer,
+		//	'incorrect answers are:',
+		//	quizIncorrectAnswers,
+		//	'All answers are',
+		//	allOptions
+		//);
+
+		return { quizQuestion, quizCorrectAnswer, quizIncorrectAnswers, allOptions };
 	} catch (error) {
 		console.error(error.message);
 	}
 }
-
-console.log(getQuiz());
 
 //Variables
 
@@ -36,39 +60,42 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public', 'index.html'));
 });
 
-io.of('/quiz').on('connect', socket => {
+io.of('/quiz').on('connect', async socket => {
 	let quizLobby = 'Player Lobby';
 	socket.join(quizLobby);
 
 	io.in(quizLobby);
 	console.log(`totalOnlineCount = ${totalOnlineCount}`);
 
-	if (totalOnlineCount === 0){
-    totalOnlineCount++
-    player = socket.id
+	if (totalOnlineCount === 0) {
+		totalOnlineCount++;
+		player = socket.id;
 		socket.emit('newPlayer', 'Player 1');
-    console.log(`${socket.id} has been chosen to be as Player!`)
-  }else{
-    totalOnlineCount++
-    spectators.push(socket.id);
+		console.log(`${socket.id} has been chosen to be as Player!`);
+
+		console.log(await getQuiz(0));
+
+		socket.emit('startGame', await getQuiz(0));
+	} else {
+		totalOnlineCount++;
+		spectators.push(socket.id);
 		socket.emit('newSpectator', 'Player ' + totalOnlineCount);
-    console.log(`${socket.id} has been moved to Spectator. (Reason: Max player is 1)`)
-  }
+		console.log(`${socket.id} has been moved to Spectator. (Reason: Max player is 1)`);
+	}
 
 	socket.on('disconnect', () => {
 		console.log(`client has left ${socket.id}`);
 
-    if (player === socket.id){
-      console.log(`Player ${socket.id} has left the lobby!`)
-      player = null
-      totalOnlineCount--
-    }else{
-      console.log(`A user ${socket.id} has left the lobby!`)
-      spectators = spectators.filter(e => e !== socket.id);
-        // console.log(spectators) Check array if successful remove correct socket.id
-      totalOnlineCount--
-    }
-
+		if (player === socket.id) {
+			console.log(`Player ${socket.id} has left the lobby!`);
+			player = null;
+			totalOnlineCount--;
+		} else {
+			console.log(`A user ${socket.id} has left the lobby!`);
+			spectators = spectators.filter(e => e !== socket.id);
+			// console.log(spectators) Check array if successful remove correct socket.id
+			totalOnlineCount--;
+		}
 	});
 });
 
--- public/client.js
+++ public/client.js
@@ -1,16 +1,26 @@
 let socket = io('/quiz');
 
+socket.on('startGame', quizData => {
+	let quizQuestion = quizData.quizQuestion;
+	let quizCorrectAnswer = quizData.quizCorrectAnswer;
+	let quizIncorrectAnswers = quizData.quizIncorrectAnswers;
+	let allOptions = quizData.allOptions;
+
+	console.log(quizQuestion, quizCorrectAnswer, quizIncorrectAnswers, allOptions);
+});
+
 socket.on('PlayerLeft', msg => {
 	console.log('Player left');
-	console.log('Client says hello there)', msg)
+	console.log('Client says hello there)', msg);
 });
+
 socket.on('newPlayer', player1 => {
-	let h1 = document.querySelector('h1');
+	let h1 = document.querySelector('h2');
 	h1.textContent = 'Question:';
 });
 
 socket.on('newSpectator', spectator => {
-	let h1 = document.querySelector('h1');
+	let h1 = document.querySelector('h2');
 	h1.textContent = 'Question is';
 });
 
