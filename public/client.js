4e60bd29a77bdbc3e9a7333ae8d48f5e7bb5d0c5
ColdFennec <xColdLight@Outlook.com>
2021-05-19 15:09:51 +0200

    32

--- public/client.js
+++ public/client.js
@@ -2,6 +2,8 @@ let socket = io('/quiz');
 
 let currentRound = 0;
 
+const OptionsChoice = document.getElementById('OptionsChoice');
+
 function nextQuestion() {
 	currentRound++;
 	socket.emit('newQuestion', currentRound);
@@ -21,10 +23,19 @@ socket.on('startGame', quizData => {
 
 	console.log(quizQuestion, quizCorrectAnswer, quizIncorrectAnswers, allOptions);
 
-	question1 = document.querySelector('#myQuestion1').value = allOptions[0];
-	question2 = document.querySelector('#myQuestion2').value = allOptions[1];
-	question3 = document.querySelector('#myQuestion3').value = allOptions[2];
-	question4 = document.querySelector('#myQuestion4').value = allOptions[3];
+	const quest = document.createElement('input')
+	quest.type = 'button'
+	quest.name="question"
+	quest.id="myQuestion1" 
+	quest.value= allOptions;
+	quest.onClick="nextQuestion()"
+
+	OptionsChoice.appendChild(quest);
+
+	// question1 = document.querySelector('#myQuestion1').value = allOptions[0];
+	// question2 = document.querySelector('#myQuestion2').value = allOptions[1];
+	// question3 = document.querySelector('#myQuestion3').value = allOptions[2];
+	// question4 = document.querySelector('#myQuestion4').value = allOptions[3];
 
 	document.querySelector('h3').textContent = quizQuestion;
 });
