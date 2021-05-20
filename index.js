2f57814ea2b007a8c5a98e729974bf9cbd8087eb
Carel Williams <carel@live.se>
2021-05-20 20:44:00 +0200

    adds new questions

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ let currentRound = 0;
 
 // Async fuction to axios.get the quiz
 async function getQuiz(round) {
-	let rounds = round;
-
 	// let quizQuestion = '';
 	let quizIncorrectAnswers = [];
 	// let quizCorrectAnswer = [];
@@ -34,12 +32,12 @@ async function getQuiz(round) {
 				params: { encode: 'url3986' },
 			}
 		);
-		//console.log(response.data.results[rounds]);
+		//console.log(response.data.results[currentRound]);
 
 		// Quiz question, answer and incorrect answers
-		quizQuestion = decodeURIComponent(response.data.results[rounds].question);
-		quizCorrectAnswer = decodeURIComponent(response.data.results[rounds].correct_answer);
-		quizIncorrectAnswers = decodeURIComponent(response.data.results[rounds].incorrect_answers);
+		quizQuestion = decodeURIComponent(response.data.results[round].question);
+		quizCorrectAnswer = decodeURIComponent(response.data.results[round].correct_answer);
+		quizIncorrectAnswers = decodeURIComponent(response.data.results[round].incorrect_answers);
 
 		allOptions.push(quizIncorrectAnswers.split(','), quizCorrectAnswer);
 		let flattenAllOptions = allOptions.flat();
@@ -56,7 +54,7 @@ async function getQuiz(round) {
 		//	'All answers are',
 		//	allOptions
 		//);
-
+		console.log('from getQuiz', allOptions);
 		return { quizQuestion, quizCorrectAnswer, quizIncorrectAnswers, allOptions };
 	} catch (error) {
 		console.error(error.message);
@@ -65,12 +63,12 @@ async function getQuiz(round) {
 
 //Shuffle the order of the array
 function shuffleArray(allOptions) {
-    for (let i = allOptions.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = allOptions[i];
-        allOptions[i] = allOptions[j];
-        allOptions[j] = temp;
-    }
+	for (let i = allOptions.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = allOptions[i];
+		allOptions[i] = allOptions[j];
+		allOptions[j] = temp;
+	}
 	console.log(allOptions);
 }
 
@@ -94,47 +92,47 @@ io.of('/quiz').on('connect', async socket => {
 		player = socket.id;
 		socket.emit('newPlayer', 'Player 1');
 		console.log(`${socket.id} has been chosen to be as Player!`);
-		console.log('On start game send: ', await getQuiz(0));
-		socket.emit('startGame', await getQuiz(0));
+		console.log('On start game send: ', await getQuiz(currentRound));
+		socket.emit('startGame', await getQuiz(currentRound));
 	}
-	// Else the rest late join will move to spectator. 
+	// Else the rest late join will move to spectator.
 	else {
 		// Move client to room as Spectator and emit it to see spectator view
 		spectators.push(socket.id);
 		let SpectatorRoom = 'Spectator';
-    socket.join(SpectatorRoom);
+		socket.join(SpectatorRoom);
 
 		socket.emit('newSpectator');
 		console.log(`${socket.id} has been moved to Spectator. (Reason: Max player is 1)`);
 	}
 
 	// Respond and check if answer was correct, then send emit to update spectator view of resulting.
-	socket.on('AnswerRespond', answer => {
-		console.log(answer)
-		console.log(quizCorrectAnswer)
+	socket.on('AnswerRespond', async answer => {
+		console.log(answer);
+		console.log(quizCorrectAnswer);
 		// If answer is correct!
 		if (answer === quizCorrectAnswer) {
-			let resultQuest = [quizQuestion, answer, true]
-			io.of('/quiz').in('Spectator').emit('Resulting', resultQuest)
+			let resultQuest = [quizQuestion, answer, true];
+			io.of('/quiz').in('Spectator').emit('Resulting', resultQuest);
 
 			currentRound++;
-			socket.emit('newQuestion', currentRound);
+			console.log(currentRound);
+			socket.emit('startGame', await getQuiz(currentRound));
 
-		// Else if wrong...
+			// Else if wrong...
 		} else {
-			
-			let resultQuest = [quizQuestion, answer, false]
-			io.of('/quiz').in('Spectator').emit('Resulting', resultQuest)
+			let resultQuest = [quizQuestion, answer, false];
+			io.of('/quiz').in('Spectator').emit('Resulting', resultQuest);
 
 			currentRound++;
-			socket.emit('newQuestion', currentRound);
+			socket.emit('startGame', await getQuiz(currentRound));
 		}
 	});
 
 	// Send new quiz
 	socket.on('newQuestion', async round => {
-		console.log(round);
-		socket.emit('newQuestionFromServer', await getQuiz(round));
+		console.log('This is the round', round);
+		socket.emit('newQuestionFromServer', await getQuiz(currentRound));
 	});
 
 	// When client disconnect
@@ -147,8 +145,8 @@ io.of('/quiz').on('connect', async socket => {
 			io.of('/quiz').in('Spectator').emit('playerLeft');
 			// Reset and who come first will be new choosen player
 			player = null;
-		
-		// Else as if NOT player but as spectator
+
+			// Else as if NOT player but as spectator
 		} else {
 			console.log(`A user ${socket.id} has left the lobby!`);
 			spectators = spectators.filter(e => e !== socket.id);
--- public/client.js
+++ public/client.js
@@ -1,36 +1,14 @@
 let socket = io('/quiz');
 const log = document.getElementById('spectatorLog');
-const headlog = document.getElementById('spectatorHeader')
-
+const headlog = document.getElementById('spectatorHeader');
 
 let correctAnswer;
+let currentRound = 1;
 
-
-// function nextQuestion(e) {
-// 	if (e === correctAnswer) {
-// 		setTimeout(() => {
-// 			currentRound++;
-// 			socket.emit('newQuestion', currentRound);
-// 			console.log('current round', currentRound);
-// 		}, 3000);
-
-// 		document.querySelector('h3 span').textContent = 'Your answer was correct!';
-// 		document.querySelector('h3 span').style.color = 'green';
-// 	} else {
-// 		document.querySelector('h3 span').textContent = 'Incorrect! Right answer is ' + correctAnswer;
-// 		document.querySelector('h3 span').style.color = 'red';
-// 		setTimeout(() => {
-// 			currentRound++;
-// 			socket.emit('newQuestion', currentRound);
-// 			console.log('current round', currentRound);
-// 		}, 3000);
-// 	}
-
-	//return currentRound;
-	// for(let quizLoop = 0; quizLoop <= 4; quizLoop++) {
-	// 	socket.emit('newQuestion', allAnswers[quizLoop]);
-	// }
-
+//return currentRound;
+// for(let quizLoop = 0; quizLoop <= 4; quizLoop++) {
+// 	socket.emit('newQuestion', allAnswers[quizLoop]);
+// }
 
 // function showCorrectAnswer() {
 // 	if (nextQuestion('showAnswer') === correctAnswer) {
@@ -38,55 +16,49 @@ let correctAnswer;
 // 	}
 // }
 
-
-
 // Function to send back value to server
 function nextQuestion(answer) {
-	socket.emit('AnswerRespond', answer.value)
+	socket.emit('AnswerRespond', answer.value);
 }
 // Resulting to answer correct or wrong to spectator
 socket.on('Resulting', resultQuest => {
+	if (resultQuest[2] === true) {
+		// Create element and add class
+		let div = document.createElement('div');
+		let spanHead = document.createElement('span');
+		let spanAnswer = document.createElement('span');
+		div.class = 'Resulting';
 
-		if (resultQuest[2] === true){
-			
-			// Create element and add class
-			let div = document.createElement('div')
-			let spanHead = document.createElement('span')
-			let spanAnswer = document.createElement('span')
-			div.class = 'Resulting';
+		// Adding string to element
+		resultAnswer = resultQuest[1] + '(Correct!)';
+		spanHead.innerHTML = resultQuest[0];
+		spanAnswer.innerHTML = resultAnswer;
 
-			// Adding string to element
-			resultAnswer = resultQuest[1] + '(Correct!)'
-			spanHead.innerHTML = resultQuest[0];
-			spanAnswer.innerHTML = resultAnswer
+		// Appendchild together creation of div
+		log.appendChild(div);
+		div.appendChild(spanHead);
+		div.appendChild(spanAnswer);
+	} else {
+		// Create element and add class
+		let div = document.createElement('div');
+		let spanHead = document.createElement('span');
+		let spanAnswer = document.createElement('span');
+		div.class = 'Resulting';
 
-			// Appendchild together creation of div
-			log.appendChild(div)
-			div.appendChild(spanHead)
-			div.appendChild(spanAnswer)
-			
-		}else{
+		// Adding string to element
+		resultAnswer = resultQuest[1] + '(Wrong!)';
+		spanHead.innerHTML = resultQuest[0];
+		spanAnswer.innerHTML = resultAnswer;
 
-			// Create element and add class
-			let div = document.createElement('div')
-			let spanHead = document.createElement('span')
-			let spanAnswer = document.createElement('span')
-			div.class = 'Resulting';
-
-			// Adding string to element
-			resultAnswer = resultQuest[1] + '(Wrong!)'
-			spanHead.innerHTML = resultQuest[0];
-			spanAnswer.innerHTML = resultAnswer
-
-			// Appendchild together creation of div
-			log.appendChild(div)
-			div.appendChild(spanHead)
-			div.appendChild(spanAnswer)
-		}
-})
-
+		// Appendchild together creation of div
+		log.appendChild(div);
+		div.appendChild(spanHead);
+		div.appendChild(spanAnswer);
+	}
+});
 
 socket.on('startGame', quizData => {
+	console.log('this is the data', quizData);
 	let quizQuestion = quizData.quizQuestion;
 	let quizCorrectAnswer = quizData.quizCorrectAnswer;
 	let quizIncorrectAnswers = quizData.quizIncorrectAnswers;
@@ -122,30 +94,6 @@ socket.on('newQuestionFromServer', quizData => {
 	answer2 = document.querySelector('#myAnswer2').value = allOptions[1];
 	answer3 = document.querySelector('#myAnswer3').value = allOptions[2];
 	answer4 = document.querySelector('#myAnswer4').value = allOptions[3];
-	
-	// Define and replace if missing like ", ', 's and more depens what missing
-	if (quizQuestion.includes('&quot;') === true & (quizQuestion.includes('&#039;s') === true)){
-		headQuiz2 = quizQuestion.replaceAll('&#039;s', "'s")
-		headQuiz = headQuiz2.replaceAll('&quot;', '"');
-	}
-	else if (quizQuestion.includes('&quot;') === true){
-		headQuiz = quizQuestion.replaceAll('&quot;', '"');
-	}
-	else if (quizQuestion.includes('&#039') === true){
-		headQuiz = quizQuestion.replaceAll('&#039;', "'");
-	}
-	else if (quizQuestion.includes('&shy;') === true){
-		headQuiz = quizQuestion.replaceAll('&shy;', "-")
-	}
-	else if (quizQuestion.includes('&#039;s')){
-		headQuiz = quizQuestion.replaceAll('&#039;s', "'s")
-	}
-	else if (quizQuestion.includes('&deg;') === true){
-		headQuiz = quizQuestion.replaceAll('&deg;', "°")
-	}
-	else {
-		headQuiz = quizQuestion
-	}
 
 	let h3 = document.createElement('h3');
 	h3.innerHTML = headQuiz;
@@ -183,10 +131,10 @@ socket.on('newSpectator', spectator => {
 
 	// Add string contain in element
 	h2.innerHTML = 'You are a Spectator';
-	span.innerHTML = 'You join spectator automatic for a reason max player is 1.'
+	span.innerHTML = 'You join spectator automatic for a reason max player is 1.';
 
 	// Due button is already loaded in html, remove them.
-	ButtonForm = document.getElementById('myForm')
+	ButtonForm = document.getElementById('myForm');
 	ButtonForm.remove();
 
 	// appendChild together to div with id spectatorHeader already in HTML
@@ -197,23 +145,18 @@ socket.on('newSpectator', spectator => {
 
 // Progress to send span spectators view of quest and player's answer
 socket.on('LogSpectator', spectator => {
-	
-
-	PlayerAnswer = quizData
-
-})
+	PlayerAnswer = quizData;
+});
 
 // TWhen player left the lobby, send message to every active clients that game ended.
 socket.on('playerLeft', spectator => {
-	let GameEnd = document.createElement('h3')
-	document.getElementById('SpecHead').innerHTML = 'The game as ended!'
-	document.getElementById('SpecSpan').innerHTML = 'Game ended because player left the game!'
-	GameEnd.innerHTML = 'Player has left the lobby.'
-	log.appendChild(GameEnd)
-	console.log("Player Left! Game End!")
+	let GameEnd = document.createElement('h3');
+	document.getElementById('SpecHead').innerHTML = 'The game as ended!';
+	document.getElementById('SpecSpan').innerHTML = 'Game ended because player left the game!';
+	GameEnd.innerHTML = 'Player has left the lobby.';
+	log.appendChild(GameEnd);
+	console.log('Player Left! Game End!');
 });
-
-
 
 //socket.on('startGame', questions => {
 //	let ul = document.getElementById('chatQuestion');
