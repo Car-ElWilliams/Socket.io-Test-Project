50348a60120172f9f5d8f9106b166b92be3f29c5
Hampus Aronsson <hampzz99@gmail.com>
2021-05-20 19:38:14 +0200

    smol visual changes

--- public/client.js
+++ public/client.js
@@ -56,9 +56,9 @@ socket.on('Resulting', resultQuest => {
 			div.class = 'Resulting';
 
 			// Adding string to element
-			resultAnswer = resultQuest[1] + '(Correct!)'
+			resultAnswer = ' ' + resultQuest[1] + ' (Correct!)'
 			spanHead.innerHTML = resultQuest[0];
-			spanAnswer.innerHTML = resultAnswer
+			spanAnswer.innerHTML = resultAnswer.bold();
 
 			// Appendchild together creation of div
 			log.appendChild(div)
@@ -74,9 +74,9 @@ socket.on('Resulting', resultQuest => {
 			div.class = 'Resulting';
 
 			// Adding string to element
-			resultAnswer = resultQuest[1] + '(Wrong!)'
+			resultAnswer = ' ' + resultQuest[1] + ' (Wrong!)'
 			spanHead.innerHTML = resultQuest[0];
-			spanAnswer.innerHTML = resultAnswer
+			spanAnswer.innerHTML = resultAnswer.bold();
 
 			// Appendchild together creation of div
 			log.appendChild(div)
